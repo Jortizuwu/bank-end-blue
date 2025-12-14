85671ff1180ca17998d4d816238bbf65e631343b
Jortizuwu <joseher2511@gmail.com>
2025-12-14 11:50:16 -0500

    feat(reactions): create crud reactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { EnvConfiguration } from './common/config/env.config';
 import { CharactersModule } from './characters/characters.module';
+import { ReactionsModule } from './reactions/reactions.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CharactersModule } from './characters/characters.module';
       process.env.MONGODB || 'mongodb://localhost:27017/prueba-db',
     ),
     CharactersModule,
+    ReactionsModule,
   ],
 })
 export class AppModule {}
